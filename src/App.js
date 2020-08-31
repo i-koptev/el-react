7e03e316e23c8b6307794563857187b6603fe0f6
Igor Koptev <igor.koptef@gmail.com>
2020-08-31 11:36:57 +0300

    Theme provider replaced index -> App

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import React from "react"
 
-import Container from "@material-ui/core/Container"
-import Typography from "@material-ui/core/Typography"
-import Box from "@material-ui/core/Box"
-import Link from "@material-ui/core/Link"
-import ProTip from "./ProTip"
+import { ThemeProvider } from "@material-ui/core/styles"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import theme from "./theme"
 
-function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {"Copyright © "}
-            <Link color="inherit" href="https://material-ui.com/">
-                Your Website
-            </Link>{" "}
-            {new Date().getFullYear()}
-            {"."}
-        </Typography>
-    )
-}
+import HomePage from "./views/HomePage"
 
 function App() {
     return (
-        <Container maxWidth="sm">
-            <Box my={4}>
-                <Typography variant="h4" component="h1" gutterBottom>
-                    Create React App v4-beta example
-                </Typography>
-                <ProTip />
-                <Copyright />
-            </Box>
-        </Container>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <HomePage />
+        </ThemeProvider>
     )
 }
 
